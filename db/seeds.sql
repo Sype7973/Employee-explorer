83f550c0ea4e8bb1f555673c939e1781f7f0da2e
unknown <adam.day7973@gmail.com>
2023-05-08 21:52:27 +1000

    updated index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,21 +7,21 @@ VALUES ('Sales'),
 
 INSERT INTO Role (title, salary, department_id)
 VALUES ('Sales Lead', 100000.00, 1),
-       ('Salesperson', 80000.00, 1),
+       ('Account Manager', 80000.00, 1),
        ('Lead Engineer', 150000.00, 2),
        ('Software Engineer', 120000.00, 2),
-       ('Accountant', 125000.00, 3),
-       ('Legal Team Lead', 250000.00, 4),
+       ('Financial Officer', 125000.00, 3),
+       ('Legal Compliance Officer', 250000.00, 4),
        ('Lawyer', 190000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('John', 'Doe', 1, NULL),
-        ('Mike', 'Chan', 2, 1),
-        ('Ashley', 'Rodriguez', 2, 1),
+VALUES  ('John', 'Smith', 1, NULL),
+        ('Mike', 'Lee', 2, 1),
+        ('Ashley', 'Stephenson', 2, 1),
         ('Adam', 'Day', 4, 2),
         ('Jacob', "Rollo", 3, NULL),
-        ('Kevin', 'Tupik', 4, 2),
-        ('Malia', 'Brown', 5, NULL),
-        ('Sarah', 'Lourd', 6, NULL),
-        ('Tom', 'Allen', 7, 3),
+        ('Kevin', 'Bacon', 4, 2),
+        ('Tory', 'Brown', 5, NULL),
+        ('Sarah', 'Ladley', 6, NULL),
+        ('Bob', 'Philips', 7, 3),
         ('Samantha', 'Jones', 7, 3);
