994a18ab77ecd67f008aa4ca451a9bd46ec413cf
unknown <adam.day7973@gmail.com>
2023-05-09 21:49:49 +1000

    updated readme, added screenshots, updated console logs

--- README.md
+++ README.md
@@ -2,66 +2,29 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+- My motivation for this project was to create a command line interface that employers could use to manage their employee database
+- I built this project to help employers manage their employee database, I also wanted to use mySQL to access a database and understand schemas
+- This project solves the problem of having to manually update a database, it allows the user to update the database through the command line
+- I learned how challenging mySQL syntax can be, I also learned how to use inquirer to create a command line interface with different cases.
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+- To install this project, clone the repository from github, then run npm install to install the dependencies, then run node index.js to start the program.
+
+
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+- To use this project, run node index.js, then select the option you would like to use, then follow the prompts
+- To remove a department or role you must first remove all employees in that department or role.
+- To remove a manager you must first assign them to another role.
+- To remove an employee you must first remove them from any roles they are assigned to. You will receive an error message if trying to remove a role that has an employee assigned to it or an employee that has an assigned manager.
 
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+![Screenshot of the command line interface in action](./img/screencap.PNG)
 
 ## Features
 
-If your project has a lot of features, list them here.
+- You can view the total budget of a department
+- You can delete a department, role, or employee
+- You can update an employee's role
 
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- db/index.js
+++ db/index.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection(
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     },
-    console.log(`Connected to the employee_db database.`)
+    console.log('\x1b[32m%s\x1b[0m', `Connected to the employee_db database.`)
   ).promise();
 
 // helper functions for adding employees, roles, and departments using 
@@ -46,7 +46,7 @@ class Department {
   
           const query = 'INSERT INTO department (name) VALUES (?)';
           const [rows, fields] = await db.query(query, [name]);
-          console.log(`Department ${name} added successfully`);
+          console.log('\x1b[32m%s\x1b[0m', `Department ${name} added successfully`);
           return rows;
         } catch (err) {
           console.log(err);
@@ -55,31 +55,39 @@ class Department {
     }
 
     static async removeDepartment() {
-        try {
-          const departments = await db.query('SELECT * FROM department');
-          const departmentChoices = departments[0].map((department) => ({
-            name: department.name,
-            value: department.id,
-          }));
-  
-          const { departmentId } = await inquirer.prompt([
-            {
-              name: 'departmentId',
-              type: 'list',
-              message: 'Select the department to remove:',
-              choices: departmentChoices,
-            },
-          ]);
-  
-          const query = 'DELETE FROM department WHERE id = ?';
-          const [rows, fields] = await db.query(query, [departmentId]);
-          console.log(`Department removed successfully`);
-          return rows;
-        } catch (err) {
-          console.log(err);
-          throw err;
+      try {
+        const departments = await db.query('SELECT * FROM department');
+        const departmentChoices = departments[0].map((department) => ({
+          name: department.name,
+          value: department.id,
+        }));
+    
+        const { departmentId } = await inquirer.prompt([
+          {
+            name: 'departmentId',
+            type: 'list',
+            message: 'Select the department to remove:',
+            choices: departmentChoices,
+          },
+        ]);
+    
+        const rolesInDepartment = await db.query('SELECT * FROM role WHERE department_id = ?', [departmentId]);
+        const roleIds = rolesInDepartment[0].map((role) => role.id);
+        if (roleIds.length > 0) {
+          const idsString = roleIds.join(',');
+          await db.query(`DELETE FROM employee WHERE department_id IN (${idsString})`);
+        } else {
+          console.log('No roleIds found to delete employees from');
         }
-        }
+        await db.query('DELETE FROM role WHERE department_id = ?', [departmentId]);
+        await db.query('DELETE FROM department WHERE id = ?', [departmentId]);
+    
+        console.log('\x1b[32m%s\x1b[0m', `Department removed successfully, along with ${rolesInDepartment[0].length} associated roles`);
+      } catch (err) {
+        console.log("Could not remove department, as it's associated with an active role");
+        throw err;
+      }
+    }
         static async viewDepartmentBudget() {
           try {
             const departments = await db.query('SELECT * FROM department');
@@ -100,7 +108,7 @@ class Department {
               // use joining statements; d.name, join role r ON e.role_id; r.department_id = d.id is a join condition which specifies we want to join the role and department 
               const query = 'SELECT d.name AS department, SUM(e.salary) AS total_budget FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id WHERE d.id = ? GROUP BY d.id';
               const [rows, fields] = await db.query(query, [departmentId]);
-              console.log(`Department budget viewed successfully`);
+              console.log('\x1b[32m%s\x1b[0m',`Department budget viewed successfully`);
               console.table(rows);
           }
           catch (err) {
@@ -159,7 +167,7 @@ class Department {
   
           const query = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
           const [rows, fields] = await db.query(query, [title, salary, departmentId]);
-          console.log(`Role ${title} added successfully`);
+          console.log('\x1b[32m%s\x1b[0m',`Role ${title} added successfully`);
           return rows;
         } catch (err) {
           console.log(err);
@@ -168,12 +176,12 @@ class Department {
     }
     static async removeRole() {
       try {
+        // Prompt the user to select a role to remove
         const roles = await db.query('SELECT * FROM role');
         const roleChoices = roles[0].map((role) => ({
           name: role.title,
           value: role.id,
         }));
-      
         const { roleId } = await inquirer.prompt([
           {
             name: 'roleId',
@@ -182,18 +190,24 @@ class Department {
             choices: roleChoices,
           },
         ]);
-        
-        const query = 'DELETE FROM role WHERE id = ?';
-        const [rows, fields] = await db.query(query, [roleId]);
-        console.log(`Role removed successfully`);
+    
+        // Check if there are any employees assigned to the selected role
+        const [employeeRows] = await db.query('SELECT COUNT(*) as count FROM employee WHERE role_id = ?', [roleId]);
+        if (employeeRows[0].count > 0) {
+          console.log(`Cannot remove role: there are ${employeeRows[0].count} employees assigned to this role.`);
+          return;
+        }
+    
+        // Delete the selected role
+        const [rows, fields] = await db.query('DELETE FROM role WHERE id = ?', [roleId]);
+        console.log('\x1b[32m%s\x1b[0m',`Role removed successfully`);
         return rows;
       } catch (err) {
-        console.log(err);
+        console.log('Could not remove role, as it is assigned to one or more employees');
         throw err;
       }
+    }
   }
-}
-
 class Employee {
     constructor(id, first_name, last_name, role_id, manager_id) {
         this.id = id;
@@ -252,7 +266,7 @@ class Employee {
     
         const insertQuery = 'INSERT INTO employee (first_name, last_name, salary, role_id, manager_id) VALUES (?, ?, ?, ?, ?)';
         const [insertRows, insertFields] = await db.query(insertQuery, [first_name, last_name, salary, role_id, manager_id]);
-        console.table(insertRows);
+        console.log('\x1b[32m%s\x1b[0m',`${first_name} ${last_name} added successfully`);
       } catch (err) {
         console.log(err);
       }
@@ -272,9 +286,9 @@ class Employee {
 
             const query = 'DELETE FROM employee WHERE id = ?';
             const [rows, fields] = await db.query(query, [id]);
-            console.table(rows);
+            console.log('\x1b[32m%s\x1b[0m', `Employee removed successfully`);
         } catch (err) {
-            console.log(err);
+            console.log('Could not remove employee, as they are assigned to one or more managers');
         }
     }
     // this function updates an employee role by selecting the employee 
@@ -312,9 +326,9 @@ class Employee {
                 },
             ]);
 
-            const query = 'UPDATE employee SET role_id AS `employee_role` = ? WHERE id = ?';
+            const query = 'UPDATE employee SET role_id = ? WHERE id = ?';
             const [rows, fields] = await db.query(query, [roleId, employeeId]);
-            console.log(`Employee role updated successfully`);
+            console.log('\x1b[32m%s\x1b[0m', `Employee role updated successfully`);
         } catch (err) {
             console.log(err);
         }
